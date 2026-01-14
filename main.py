6183c67daf075c9cc0bfbfde507adb6e899d1dc2
bazarmatik-design <bazarmatik@gmail.com>
2026-01-15 03:18:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 import os
-import google.generativeai as genai
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
+from google import genai
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+client = genai.Client(api_key=GEMINI_API_KEY)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Salam! 🤖 Komekci Trader ✅ Sorag ýaz!")
+    await update.message.reply_text("Salam! 🤖 Men Gemini AI bot ✅ Sorag ýaz!")
+
 
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
+    user_text = update.message.text
     try:
-        r = model.generate_content(text)
+        r = client.models.generate_content(
+            model="gemini-1.5-flash",
+            contents=user_text
+        )
         await update.message.reply_text(r.text)
     except Exception as e:
         await update.message.reply_text(f"⚠️ Ýalňyşlyk: {e}")
 
+
 def main():
     app = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
+
     print("✅ Bot işe başlady...")
     app.run_polling(drop_pending_updates=True)
+
 
 if __name__ == "__main__":
     main()
